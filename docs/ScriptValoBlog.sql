76df15e0502c0102733681792f2d28d677253137
MatteoDacunha24 <100850438+MatteoDacunha24@users.noreply.github.com>
2023-06-23 16:58:14 +0200

    Update ScriptValoBlog.sql

--- docs/ScriptValoBlog.sql
+++ docs/ScriptValoBlog.sql
@@ -15,7 +15,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema mydb
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `valoblog` DEFAULT CHARACTER SET utf8 ;
-USE `valoblog` ;
+USE `valoblog`;
 
 -- -----------------------------------------------------
 -- Table `mydb`.`characters`
@@ -79,4 +79,4 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 
 CREATE USER 'valoblog'@localhost IDENTIFIED BY 'Pa$$w0rd';
-GRANT SELECT,INSERT,DELETE,UPDATE ON valoblog. * TO 'valoblog'@localhost IDENTIFIED BY 'Pa$$w0rd';
+GRANT SELECT,INSERT,DELETE,UPDATE ON valoblog.* TO 'valoblog'@localhost IDENTIFIED BY 'Pa$$w0rd';
